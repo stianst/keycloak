7958ee48fdb22f8fdc0fc6791d3ae1bbe2d62148
Stan Silvert <ssilvert@redhat.com>
2017-04-28 13:21:07 -0400

    KEYCLOAK-1250: New Acct Mgt Console - initial commit

---
+++ themes/src/main/resources/theme/keycloak-preview/account/resources/app/keycloak-service/keycloak.service.ts
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// <reference path="keycloak.d.ts"/>
+
+import {Injectable} from '@angular/core';
+import InitOptions = KeycloakModule.InitOptions;
+
+// Need to use local keycloak.js for now due to Chrome bug.
+// This is a newer version than the one obtained from the server.
+var Keycloak = require("./keycloak"); // load keycloak.js locally
+
+type KeycloakClient = KeycloakModule.KeycloakClient;
+
+@Injectable()
+export class KeycloakService {
+    static keycloakAuth: KeycloakClient;
+
+    /**
+     * Configure and initialize the Keycloak adapter.
+     *
+     * @param configOptions Optionally, a path to keycloak.json, or an object containing
+     *                      url, realm, and clientId.
+     * @param adapterOptions Optional initiaization options.  See javascript adapter docs
+     *                       for details.
+     * @returns {Promise<T>}
+     */
+    static init(configOptions?: any, adapterOptions?: InitOptions): Promise<any> {
+        KeycloakService.keycloakAuth = Keycloak(configOptions);
+
+        return new Promise((resolve, reject) => {
+            KeycloakService.keycloakAuth.init(adapterOptions)
+                .success(() => {
+                    resolve();
+                })
+                .error((errorData: any) => {
+                    reject(errorData);
+                });
+        });
+    }
+
+    authenticated(): boolean {
+        return KeycloakService.keycloakAuth.authenticated;
+    }
+
+    login() {
+        KeycloakService.keycloakAuth.login();
+    }
+
+    logout() {
+        KeycloakService.keycloakAuth.logout();
+    }
+
+    account() {
+        KeycloakService.keycloakAuth.accountManagement();
+    }
+
+    getToken(): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            if (KeycloakService.keycloakAuth.token) {
+                KeycloakService.keycloakAuth
+                    .updateToken(5)
+                    .success(() => {
+                        resolve(<string>KeycloakService.keycloakAuth.token);
+                    })
+                    .error(() => {
+                        reject('Failed to refresh token');
+                    });
+            } else {
+                reject('Not loggen in');
+            }
+        });
+    }
+}
