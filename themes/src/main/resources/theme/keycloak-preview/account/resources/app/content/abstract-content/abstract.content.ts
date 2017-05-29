f6203a8c09ac15a1549a869e50f5914a71d19497
Stan Silvert <ssilvert@redhat.com>
2017-05-29 14:14:54 -0400

    Add error notification, save account changes, improve error handling.

--- themes/src/main/resources/theme/keycloak-preview/account/resources/app/content/abstract-content/abstract.content.ts
+++ themes/src/main/resources/theme/keycloak-preview/account/resources/app/content/abstract-content/abstract.content.ts
@@ -15,37 +15,70 @@
  * limitations under the License.
  */
 import {Http, Response} from '@angular/http';
- 
+
+import {ToastNotifier, ToastNotification} from '../../top-nav/toast.notifier';
 import {KeycloakService} from '../../keycloak-service/keycloak.service';
- 
- export abstract class AbstractContent {
+
+export abstract class AbstractContent {
     private accountUrl: string;
-     
-    constructor(protected http: Http, protected kcSvc: KeycloakService) {
+
+    constructor(protected http: Http,
+        protected kcSvc: KeycloakService,
+        protected notifier: ToastNotifier) {
         this.accountUrl = kcSvc.authServerUrl() + '/realms/' + kcSvc.realm() + '/account';
     }
-    
+
     protected doGetRequest(endpoint: string) {
         this.http.get(this.accountUrl + endpoint)
             .subscribe((res: Response) => this.handleGetResponse(res),
-                       (error: Response) => this.handleServiceError(error));
+            (error: Response) => this.handleServiceError(error));
     }
-    
+
     protected abstract handleGetResponse(res: Response): void;
-    
+
     protected doPostRequest(endpoint: string, body: any) {
         this.http.post(this.accountUrl + endpoint, body)
-            .subscribe((res: Response) => this.handlePostResponse(res),
-                       (error: Response) => this.handleServiceError(error));
+            .subscribe((res: Response) => this.doPostSuccess(res),
+            (error: Response) => this.handleServiceError(error));
     }
-    
+
+    private doPostSuccess(res: Response): void {
+        this.notifier.emit(new ToastNotification("Your account has been updated.", "success"));
+        this.handlePostResponse(res);
+    }
+
     protected abstract handlePostResponse(res: Response): void;
-    
-    private handleServiceError(error: Response) {
-      console.log('**** ERROR!!!! ***');
-      console.log(JSON.stringify(error));
-      console.log('***************************************')
+
+    private handleServiceError(response: Response) {
+        console.log('**** ERROR!!!! ***');
+        console.log(JSON.stringify(response));
+        console.log('***************************************')
+        
+        if (response.status === 401) {
+            this.kcSvc.logout();
+            return;
+        }
+
+        if (response.status === 403) {
+            // TODO: go to a forbidden page?
+        }
+
+        if (response.status === 404) {
+            // TODO: route to PageNotFoundComponent
+        }
+
+        let message: string = response.status + " " + response.statusText;
+
+        // Unfortunately, errors can be sent back in the response body as
+        // 'errorMessage' or 'error_description'
+        if (response.json().hasOwnProperty('errorMessage')) {
+            message = response.json().errorMessage;
+        }
+
+        if (response.json().hasOwnProperty('error_description')) {
+            message = response.json().error_description;
+        }
+
+        this.notifier.emit(new ToastNotification(message, "error"));
     }
- }
-
-
+}
