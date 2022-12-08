305676fbc6a2f1ebf77b89d15c4af4fb62814004
Stian Thorgersen <stian@redhat.com>
2022-12-08 22:44:49 +0100

    Quotes in suite.sh

--- testsuite/integration-arquillian/tests/base/testsuites/suite.sh
+++ testsuite/integration-arquillian/tests/base/testsuites/suite.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
-GROUP=$1
+GROUP="$1"
 if [ "$GROUP" == "" ]; then
   echo "Usage: suite.sh database|fips"
   exit
 fi
 
 DIR=`readlink -f $0 | xargs dirname`
-cd $DIR
+cd "$DIR"
 
 TESTSUITE_FILE="$GROUP-suite"
 
 TESTS=`cat $TESTSUITE_FILE`
 
-echo $TESTS | sed 's/ /,/g'
+echo "$TESTS" | sed 's/ /,/g'
