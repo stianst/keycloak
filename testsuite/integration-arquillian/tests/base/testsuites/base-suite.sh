313cd2b0e9481b592976536d43e5ba5c54e16ff4
stianst <stianst@gmail.com>
2022-12-09 09:06:10 +0100

    Improve testsuite scripts

--- testsuite/integration-arquillian/tests/base/testsuites/base-suite.sh
+++ testsuite/integration-arquillian/tests/base/testsuites/base-suite.sh
@@ -1,21 +1,41 @@
 #!/bin/bash -e
 
+DIR=`readlink -f $0 | xargs dirname`
 GROUP=$1
+TESTSUITE_FILE="$DIR/base-suite"
+TEST_DIR="$DIR/../src/test/java/org/keycloak/testsuite"
+
 if [ "$GROUP" == "" ]; then
   echo "Usage: base-suite.sh <group>"
   exit
 fi
 
-DIR=`readlink -f $0 | xargs dirname`
-cd $DIR
+PACKAGES=`cat $TESTSUITE_FILE | grep -v '^[[:space:]]*$' | grep -v '^[[:space:]]*#'`
 
-TESTSUITE_FILE="base-suite"
-TESTS=`cat $TESTSUITE_FILE | grep ",$GROUP" | sed "s/,$GROUP//"`
-TESTS=`echo $TESTS | sed 's/ /|/g'`
+# Check all packages in testsuite are included
+for i in `ls -d $TEST_DIR/*/ | sed "s|$TEST_DIR||g" | sed "s|/||g"`; do
+  if ( ! cat $TESTSUITE_FILE | grep "^$i," >/dev/null ); then
+    echo "Test category '$i' not defined in base-suite"
+    exit 1
+  fi
+done
 
-if [ "$TESTS" == "" ]; then
-  echo "Undefined group '$GROUP'"
-  exit 1
-fi
+SEP=""
+GROUP_PACKAGES=""
+for i in `echo $PACKAGES`; do
+  PACKAGE=`echo $i | cut -d ',' -f 1`
+  PACKAGE_GROUP=`echo $i | cut -d ',' -f 2`
 
-echo "%regex[org.keycloak.testsuite.($TESTS).*]"
+  # Check package exists
+  if [ ! -d "$TEST_DIR/$PACKAGE" ]; then
+    echo "$PACKAGE not found in org/keycloak/testsuite"
+    exit 1
+  fi
+
+  if [ "$GROUP" == "$PACKAGE_GROUP" ]; then
+    GROUP_PACKAGES="$GROUP_PACKAGES$SEP$PACKAGE"
+    SEP='|'
+  fi
+done
+
+echo "%regex[org.keycloak.testsuite.($GROUP_PACKAGES).*]"
--- testsuite/integration-arquillian/tests/base/testsuites/suite.sh
+++ testsuite/integration-arquillian/tests/base/testsuites/suite.sh
@@ -1,13 +1,26 @@
 #!/bin/bash -e
 
+DIR=`readlink -f $0 | xargs dirname`
 GROUP="$1"
-if [ "$GROUP" == "" ]; then
-  echo "Usage: suite.sh database|fips"
-  exit
+TEST_DIR="$DIR/../src/test/java/"
+SUITE_FILE=$DIR/$GROUP-suite
+
+if [ ! -f "$SUITE_FILE" ]; then
+  echo "Suite '$1' not found"
+  exit 1
 fi
 
-DIR=`readlink -f $0 | xargs dirname`
+SEP=""
+for i in `cat "$SUITE_FILE" | grep -v '^[[:space:]]*$' | grep -v '^[[:space:]]*#'`; do
+  # Check test exists, ignoring checking packages for now
+  if [[ "$i" != *'.'* ]]; then
+    SEARCH=`find "$TEST_DIR" -name "$i.java"`
+    if [ "$SEARCH" == "" ]; then
+      echo "$i not found in testsuite"
+      exit 1
+    fi
+  fi
 
-TESTS=`cat $DIR/$GROUP-suite`
-
-echo "$TESTS" | sed 's/\n/,/g'
+  echo -n "$SEP$i"
+  SEP=","
+done
