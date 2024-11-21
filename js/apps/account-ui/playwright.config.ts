8bc6557cf96d2b43eb4251de75c5afb6e57f44f7
Stian Thorgersen <stian@redhat.com>
2024-11-21 15:08:26 +0100

    Update playwright.config.ts

--- js/apps/account-ui/playwright.config.ts
+++ js/apps/account-ui/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   testDir: "./test",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: 1,
   reporter: process.env.CI ? [["github"], ["html"]] : "list",
   expect: {
